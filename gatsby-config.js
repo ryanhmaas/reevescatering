3961d07ff996772b867a7d520d712c222d9e95be
Ryan Maas <ryan.h.maas@gmail.com>
2019-12-07 11:15:47 -0500

    Netlify cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,8 @@ module.exports = {
       },
     },
     'gatsby-plugin-sitemap',
-    'gatsby-plugin-robots-txt'
+    'gatsby-plugin-robots-txt',
+    'gatsby-plugin-netlify-cache'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
