b814df270b438232172cc24b84afdd75143a5c95
Ryan Maas <ryan.h.maas@gmail.com>
2020-04-25 12:19:20 -0400

    comment

---
+++ src/templates/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "../components/layout"
+
+//f
+function Page(){
+  return (
+    <Layout>
+      <div>content here</div>
+    </Layout>
+  )
+}
+
+export default Page;
