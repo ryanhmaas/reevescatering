525d0f08f4b367fe74d11b16b9ef845ae0dbd3ce
Ryan Maas <ryan.h.maas@gmail.com>
2020-05-19 17:44:39 -0400

    index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,13 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import '../../assets/styles/main.scss';
 
+import SectionGrid from '../components/SectionGrid';
+import SectionInstagram from '../components/SectionInstagram';
+import SectionSlider from '../components/SectionSlider';
+import { CONTENTFUL_SECTION_TYPES } from '../constants/enums';
+import SectionCTA from '../components/SectionCTA';
+import SectionContent from '../components/SectionContent';
+
 const INDEX_PAGE_QUERY = graphql`
 	query IndexPageDataQuery {
 		homePage: contentfulPageSingle(slug: { eq: "home" }) {
@@ -134,7 +141,29 @@ const IndexPage = () => (
 		<StaticQuery
 			query={INDEX_PAGE_QUERY}
 			render={({ homePage }) => {
-				return <div className="columns is-multiline">{JSON.stringify(homePage)}</div>;
+				return (
+					<div className="columns is-multiline">
+						{homePage.pageSections.map((section) => {
+							let typeName = section['__typename'];
+							switch (typeName) {
+								case CONTENTFUL_SECTION_TYPES.CONTENT:
+									return <SectionContent data={section} />;
+								case CONTENTFUL_SECTION_TYPES.CTA:
+									return <SectionCTA data={section} />;
+								case CONTENTFUL_SECTION_TYPES.DIVIDER:
+									return <div>Divider component here</div>;
+								case CONTENTFUL_SECTION_TYPES.GRID:
+									return <SectionGrid data={section} />;
+								case CONTENTFUL_SECTION_TYPES.INSTAGRAM:
+									return <SectionInstagram />;
+								case CONTENTFUL_SECTION_TYPES.SLIDER:
+									return <SectionSlider data={section} />;
+								default:
+									return <div />;
+							}
+						})}
+					</div>
+				);
 			}}
 		/>
 		<Link to="/page-2/">Go to page 2</Link>
