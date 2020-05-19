0a94217dab084b0a9411476e43ae5a461dc052ca
Ryan Maas <ryan.h.maas@gmail.com>
2020-05-19 17:55:48 -0400

    fix footer

--- src/components/HyperionFooter.js
+++ src/components/HyperionFooter.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
 const HyperionFooter = (props) => {
-  return (
-    <div className="hyperion--footer">
-      <div className="hyperion--footer-reeves">&copy; Reeves Catering 2019</div>
-      <div className="hyperion--detail">
-        <img 
-          alt="Hyperion Development Logo"
-          className="hyperion--detail-logo" 
-          title="Placeholder Image"
-          src={props.logo.publicURL}
-          />
-        <div>Designed and Developed by <a className="hyperion--detail-sitelink" href="https://www.hyperiondevelopment.net" target="_blank" rel="noreferrer noopener">Hyperion Development LLC</a></div>
-      </div>
-
-    </div>
-  )
-}
+	return (
+		<div className="hyperion__footer">
+			<div className="hyperion__footer-reeves">&copy; Reeves Catering 2019</div>
+			<div className="hyperion__detail">
+				<img
+					alt="Hyperion Development Logo"
+					className="hyperion__detail-logo"
+					title="Placeholder Image"
+					src={props.logo.publicURL}
+				/>
+				<div>
+					Designed and Developed by{' '}
+					<a
+						className="hyperion__detail-sitelink"
+						href="https://www.hyperiondevelopment.net"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						Hyperion Development LLC
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default HyperionFooter;
