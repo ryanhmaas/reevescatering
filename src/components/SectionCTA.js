7aad60b9be36483132057c7a00004500e76a850b
Ryan Maas <ryan.h.maas@gmail.com>
2020-05-19 17:49:51 -0400

    CTA section

--- src/components/SectionCTA.js
+++ src/components/SectionCTA.js
@@ -3,16 +3,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/pro-solid-svg-icons';
 function SectionCTA(props) {
 	const { data } = props;
-	console.log(data);
 	const { callToActionBorder, callToActionTitle, callToActionButton } = data;
 
+	const formattedBorderName = callToActionBorder.toLowerCase();
 	const { linkName, linkIcon, linkIconPlacement } = callToActionButton;
+
+	const formattedLinkIconPlacement = linkIconPlacement.toLowerCase();
+	console.log(linkIconPlacement);
 	return (
-		<div className="section-cta">
+		<div className={`section-cta -border-${formattedBorderName}`}>
 			<div className="section-cta__title">{callToActionTitle}</div>
 			<button className="section-cta__btn">
+				{formattedLinkIconPlacement === 'left' && (
+					<FontAwesomeIcon className="mr-1/2" icon={faPaperPlane} style={{ color: '#fff' }} />
+				)}
 				{linkName}
-				<FontAwesomeIcon icon={faPaperPlane} style={{ color: '#fff' }} />
+				{formattedLinkIconPlacement === 'right' && (
+					<FontAwesomeIcon className="ml-1/2" icon={faPaperPlane} style={{ color: '#fff' }} />
+				)}
 			</button>
 		</div>
 	);
