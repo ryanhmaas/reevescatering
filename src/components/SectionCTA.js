bae050b192b0d852c09eaf1d5e4cfbd922259bc5
Ryan Maas <ryan.h.maas@gmail.com>
2020-05-19 17:04:52 -0400

    dash bullshit

--- src/components/SectionCTA.js
+++ src/components/SectionCTA.js
@@ -1,21 +1,21 @@
-import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPaperPlane } from "@fortawesome/pro-solid-svg-icons"
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/pro-solid-svg-icons';
 function SectionCTA(props) {
-  const { data } = props
-  console.log(data)
-  const { callToActionBorder, callToActionTitle, callToActionButton } = data
+	const { data } = props;
+	console.log(data);
+	const { callToActionBorder, callToActionTitle, callToActionButton } = data;
 
-  const { linkName, linkIcon, linkIconPlacement } = callToActionButton
-  return (
-    <div className="section--cta">
-      <div className="section--cta__title">{callToActionTitle}</div>
-      <button className="section--cta__btn">
-        {linkName}
-        <FontAwesomeIcon icon={faPaperPlane} style={{ color: "#fff" }} />
-      </button>
-    </div>
-  )
+	const { linkName, linkIcon, linkIconPlacement } = callToActionButton;
+	return (
+		<div className="section-cta">
+			<div className="section-cta__title">{callToActionTitle}</div>
+			<button className="section-cta__btn">
+				{linkName}
+				<FontAwesomeIcon icon={faPaperPlane} style={{ color: '#fff' }} />
+			</button>
+		</div>
+	);
 }
 
-export default SectionCTA
+export default SectionCTA;
--- src/components/SectionGrid.js
+++ src/components/SectionGrid.js
@@ -1,42 +1,42 @@
-import React from "react"
-import classNames from "classnames"
-import ItemVenue from "./ItemVenue"
-import ItemIcon from "./ItemIcon"
-import ItemEmployee from "./ItemEmployee"
+import React from 'react';
+import classNames from 'classnames';
+import ItemVenue from './ItemVenue';
+import ItemIcon from './ItemIcon';
+import ItemEmployee from './ItemEmployee';
 
 function SectionGrid({ data }) {
-  const { displayGridName, gridName, gridItems, gridType } = data
+	const { displayGridName, gridName, gridItems, gridType } = data;
 
-  // todo - move switch checks to constants
-  const gridItem = props => {
-    if (gridType === "VenueGrid") {
-      return <ItemVenue {...props} />
-    } else if (gridType === "EmployeeGrid") {
-      return <ItemEmployee {...props} />
-    } else {
-      return <ItemIcon {...props} />
-    }
-  }
+	// todo - move switch checks to constants
+	const gridItem = (props) => {
+		if (gridType === 'VenueGrid') {
+			return <ItemVenue {...props} />;
+		} else if (gridType === 'EmployeeGrid') {
+			return <ItemEmployee {...props} />;
+		} else {
+			return <ItemIcon {...props} />;
+		}
+	};
 
-  const showGridItems = gridItems.length > 0
-  return (
-    <section className="grid--section">
-      {"grid name: " + gridName}
-      {"display grid:" + displayGridName}
-      {"item length" + gridItems.length}
-      {showGridItems && (
-        <div
-          className={classNames("grid", {
-            "employee--grid": gridType === "EmployeeGrid",
-          })}
-        >
-          {gridItems.map((item, index) => {
-            return gridItem(item)
-          })}
-        </div>
-      )}
-    </section>
-  )
+	const showGridItems = gridItems.length > 0;
+	return (
+		<section className="grid-section">
+			{'grid name: ' + gridName}
+			{'display grid:' + displayGridName}
+			{'item length' + gridItems.length}
+			{showGridItems && (
+				<div
+					className={classNames('grid', {
+						'employee-grid': gridType === 'EmployeeGrid'
+					})}
+				>
+					{gridItems.map((item, index) => {
+						return gridItem(item);
+					})}
+				</div>
+			)}
+		</section>
+	);
 }
 
-export default SectionGrid
+export default SectionGrid;
