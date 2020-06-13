979c9c10eefd5ad028ff201fe709d4766385cef3
dereknelsen <hyperiondevanddesign@gmail.com>
2020-06-13 11:39:44 -0400

    update form styles and contact page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -24,49 +24,58 @@ function ContactForm() {
 		setFormSubmitted(true);
 	};
 
+	const onChange = (event) => {
+		if (event.target.value){
+			event.target.classList.add('-active');
+		}
+		else {
+			event.target.classList.remove('-active');
+		}
+	};
+
 	return (
 		<div className="contact-section">
 			<h2>Want to Learn More?</h2>
 			<p>
 				We would love the opportunity to sit down and discuss your important event! Contact us today using the
-				form, or call Larry at <span className="contact-section__phone">864.275.0021</span>
+				form, or call Larry at <span className="contact-section__phone">864-275-0021</span>
 			</p>
 			<form onSubmit={handleSubmit(onSubmit)} className="contact-form">
-				<div className="contact-group contact-group__name">
-					<div className="form-group">
+				<div className="u-contact-group contact-group__name">
+					<div className="u-form-group">
+						<input className="u-form-group__input" name="nameRequired" type="text" onChange={onChange} placeholder="Name" ref={register({ required: true })} />
 						<label for="nameRequired">Name</label>
-						<input name="nameRequired" placeholder="Name" ref={register({ required: true })} />
 						{errors.nameRequired && <span className="field-required">This field is required</span>}
 					</div>
-					<div className="form-group">
-						<label for="phoneNumber">Phone Number</label>
-						<input name="phoneNumber" placeholder="Phone Number" ref={register} />
+					<div className="u-form-group">
+						<input className="u-form-group__input" name="phoneNumber" type="tel" onChange={onChange} pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone Number" ref={register} />
+						<label for="phoneNumber">Phone Number <span>(555-555-5555)</span></label>
 					</div>
 				</div>
-				<div className="contact-group">
-					<div className="form-group">
-						<label for="emailRequired">Email</label>
-						<input name="emailRequired" ref={register({ required: true })} />
+				<div className="u-contact-group">
+					<div className="u-form-group">
+						<input className="u-form-group__input" name="emailRequired" type="email" onChange={onChange}  ref={register({ required: true })} />
+						<label for="emailRequired">Email <span>(name@email.com)</span></label>
 						{errors.emailRequired && <span className="field-required">This field is required</span>}
 					</div>
 				</div>
-				<div className="contact-group">
-					<div className="form-group">
+				<div className="u-contact-group">
+					<div className="u-form-group">
+						<textarea className="u-form-group__input" name="messageRequired" onChange={onChange}  ref={register({ required: true })} />
 						<label for="messageRequired">Message</label>
-						<textarea name="messageRequired" ref={register({ required: true })} />
 						{errors.messageRequired && <span className="field-required">This field is required</span>}
 					</div>
 				</div>
 				<button type="submit" className="c-btn contact-form__btn">
 					{formSubmitted && (
-						<span className="btn-message">
+						<>
 							Message Sent! <FontAwesomeIcon icon={[ 'fal', 'check' ]} />
-						</span>
+						</>
 					)}
 					{!formSubmitted && (
-						<span className="btn-message">
+						<>
 							Send Message <FontAwesomeIcon icon={faPaperPlane} />
-						</span>
+						</>
 					)}
 				</button>
 			</form>
