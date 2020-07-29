f808c62da69f1efda1e9f02cef3a489ae16d05af
Ryan Maas <ryan.h.maas@gmail.com>
2020-07-29 14:14:10 -0400

    try libre baskerville

--- constants/styles.js
+++ constants/styles.js
@@ -1,16 +1,16 @@
 export const styles = {
-  colors: {
-    basicallyBlack: '#1C1D20',
-    darkGrey: '#36383D',
-    lightGrey: '#E5E5E5',
-    mediumGrey: '#555555',
-    reevesLightGreen: '#7AC142',
-    reevesGreen: '#60973A',
-    white: '#FFF',
-  },
-  fonts: {
-    euphoriaScript: 'Euphoria Script',
-    openSans: 'Open Sans',
-    openSansCondensed: 'Open Sans Condensed'
-  }
-}
+	colors: {
+		basicallyBlack: '#1C1D20',
+		darkGrey: '#36383D',
+		lightGrey: '#E5E5E5',
+		mediumGrey: '#555555',
+		reevesLightGreen: '#7AC142',
+		reevesGreen: '#60973A',
+		white: '#FFF'
+	},
+	fonts: {
+		euphoriaScript: 'Libre Baskerville',
+		openSans: 'Open Sans',
+		openSansCondensed: 'Open Sans Condensed'
+	}
+};
