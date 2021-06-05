167cba0a30b74595f48aab6526c132e749dd4477
dereknelsen <hyperiondevanddesign@gmail.com>
2021-06-05 12:47:54 -0400

    fix mobile issues, update colors, update typography and spacing

--- src/components/ItemRichText.js
+++ src/components/ItemRichText.js
@@ -12,7 +12,6 @@ function ItemRichText(props) {
     <>
       <div
         className="c-rich-text"
-        style={{ textAlign: justify }}
         dangerouslySetInnerHTML={{ __html: html }}
       ></div>
       <div className="c-rich-text__c-btn-wrapper">
